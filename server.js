9007237a09108e677d5af3c917983cdf504135e3
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-08-01 21:29:53 +0530

    feat(redis): add Redis-backed shared rate limit counter

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const redis = require('./src/redisClient');
 const app = express();
 
 const hits = {};
 const LIMIT = process.env.LIMIT || 5;
-
+const WINDOW = process.env.WINDOW || 60;
 app.get("/health", (req, res) => {
   res.json({
     status: "ok"
@@ -32,6 +33,31 @@ app.get("/ping", (req, res) => {
     requestCount: hits[ip],
     remaining: LIMIT - hits[ip]
   })
+})
+
+app.get("/pingRedis", async(req, res) => {
+  const ip = req.ip;
+  const key = `rate_limit:${ip}`;
+  const count = await redis.incr(key);
+
+  if(count === 1){
+    await redis.expire(key, WINDOW);
+  }
+  if(count > LIMIT){
+    return res.status(429).json({
+      message: "Too many Requests",
+      yourIP: ip,
+      totalRequests: count
+    }) 
+  }
+
+  res.json({
+    message: "pong!",
+    yourIP: ip,
+    requestCount: count,
+    remaining: LIMIT - count
+  })
+
 })
 
 const PORT = process.env.PORT || 3001;
